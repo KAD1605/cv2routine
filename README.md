4734600323166df2feee053c5ffea3cf5fd2ef09
Andrey <33835790+KAD1605@users.noreply.github.com>
2022-09-21 14:37:01 +0300

    Added information to the README

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # CV2-Routine
 ___
-This package simplifies the routine in OpenCV for Python 3
+>This package makes some routine operations in OpenCV easier. Also this project is used for learning, so do not use it in serious projects, as it is impossible to guarantee the performance of this package
+
+Current status: **In development**
+___
+# Documentation
+Documentation is available [here](https://cv2routine.readthedocs.io/en/latest/)
+
+(WIP. Will be ready in a while)
+
+___
+## Installation
+```bash
+pip install cv2routine
+```
+___
+## Usage
+For the complete package
+```python  
+import cv2routine as cv2r
+```
+For a specific module
+```python
+from cv2routine import transform as tf
+```
+___
+## Dependencies
+* OpenCV
+
--- setup.py
+++ setup.py
@@ -1,20 +1,29 @@
 from setuptools import setup
+
 # import os
 
+SHORT_DESCRIPTION = 'Helps with routines in OpenCV'
 
 # def requirements():
 #     with open(os.path.join(os.path.dirname(__file__), 'requirements.txt'), encoding='utf-8') as f:
 #         return f.read().splitlines()
+try:
+    with open('README.md', encoding='utf8') as readme:
+        LONG_DESCRIPTION = readme.read()
 
+except FileNotFoundError:
+    LONG_DESCRIPTION = SHORT_DESCRIPTION
 
 setup(install_requires=[
     "setuptools==65.3.0",
     "opencv-python==4.6.0.66",
     "numpy==1.23.3",
-    ],
+],
     name='cv2routine',
-    version='0.0.1',
-    description='Helps with routines in OpenCV',
+    version='0.0.2',
+    description=SHORT_DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
     author="Klinovitskij Andrey",
     author_email="adk160501@gmail.com",
     url="",
